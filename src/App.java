42026a9ef8bde02445acb4dfb06726941e6209ab
BartoszLiberda <C00295791@setu.ie>
2025-04-09 14:39:24 +0100

    comit

--- src/App.java
+++ src/App.java
@@ -1,7 +1,23 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import model.Ticket;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Ticket ticket = new Ticket("X", "Y", "Z");
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1200, 800);
+
+        JPanel cardPanel = new JPanel(new CardLayout());
+
+        JPanel login = new JPanel(); // edit this 
+        JPanel signup = new JPanel(); // this too 
+
+        cardPanel.add(login, "login");
+        cardPanel.add(signup, "sign up")
+
+        frame.add(cardPanel);
+        frame.setVisible(true);
     }
 }
